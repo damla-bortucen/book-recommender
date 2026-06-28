d4b95732ced81e63c1048ebc184c6f91252b6bb1
Damla Bortucen <damla.bortucen.24@ucl.ac.uk>
2026-06-28 15:53:08 +0100

    feat: return html on the homepage

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI() # the application object
 # the server (uvicorn) runs THIS object.
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {"message": "book recommender server!"}
+    return """
+    <!doctype html>
+    <html>
+      <head><title>Book Recommender</title></head>
+      <body>
+        <h1>Book Recommender</h1>
+        <p>The server is alive!</p>
+      </body>
+    </html>
+    """
 
 # run with uvicorn app:app --reload (reload makes server autostart when you make a change)
